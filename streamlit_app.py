80a74797b5af5d5345489986fb04bf9d64032d06
dusanBirta <31896340+dusanBirta@users.noreply.github.com>
2023-07-14 17:32:03 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,53 @@
 import streamlit as st
+import cv2
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+from PIL import Image
 
-picture = st.camera_input("Take a picture")
+# Load pre-trained model
+model = load_model('rock_paper_scissors_cnn.h5')
 
-if picture:
-    st.image(picture)
+# Function to preprocess the image
+def preprocess_image(image):
+    img = cv2.resize(image, (128, 128))
+    img = tf.cast(img, tf.float32) / 255.0
+    img = np.expand_dims(img, axis=0)
+    return img
+
+# Function to predict the gesture
+def predict_gesture(image):
+    preprocessed_image = preprocess_image(image)
+    prediction = model.predict(preprocessed_image)
+    predicted_class = np.argmax(prediction)
+    return predicted_class
+
+# Streamlit app
+def main():
+    # Set page title
+    st.title("Image Classification")
+
+    # Capture image from webcam
+    picture = st.camera_input("Take a picture")
+
+    # Perform prediction if an image is captured
+    if picture is not None:
+        # Convert the captured image to OpenCV format
+        image = np.array(picture)
+
+        # Display the captured image
+        st.image(image, channels="RGB", use_column_width=True)
+
+        # Predict gesture
+        gesture = predict_gesture(image)
+
+        # Display the predicted gesture
+        if gesture == 0:
+            st.write("You made a Rock!")
+        elif gesture == 1:
+            st.write("You made a Paper!")
+        elif gesture == 2:
+            st.write("You made Scissors!")
+
+if __name__ == '__main__':
+    main()
