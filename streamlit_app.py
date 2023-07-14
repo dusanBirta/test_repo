6994cc62c3ef1f309ff0829a3fd9298309c64224
dusanBirta <31896340+dusanBirta@users.noreply.github.com>
2023-07-14 17:26:10 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,33 +1,6 @@
 import streamlit as st
-import cv2
-import numpy as np
-from PIL import Image
 
-# Function to capture image from webcam
-def capture_image():
-    cap = cv2.VideoCapture(0)
-    ret, frame = cap.read()
-    cap.release()
-    return frame
+picture = st.camera_input("Take a picture")
 
-# Streamlit app
-def main():
-    # Set page title
-    st.title("Image Capture")
-
-    # Button to capture image
-    if st.button("Capture Image"):
-        # Capture the image
-        image = capture_image()
-
-        # Convert the color channels from BGR to RGB
-        rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-
-        # Convert the captured image to PIL format
-        pil_image = Image.fromarray(rgb_image)
-
-        # Display the captured image
-        st.image(pil_image, channels="RGB", use_column_width=True)
-
-if __name__ == '__main__':
-    main()
+if picture:
+    st.image(picture)
