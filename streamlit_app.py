b144ae73c128c50f66b54884247983bd8f94b3b3
dusanBirta <31896340+dusanBirta@users.noreply.github.com>
2023-07-14 20:33:40 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,9 +43,9 @@ def main():
 
         # Display the predicted gesture
         if gesture == 0:
-            st.write("You made a Rock!")
+            st.write("You made Rock!")
         elif gesture == 1:
-            st.write("You made a Paper!")
+            st.write("You made Paper!")
         elif gesture == 2:
             st.write("You made Scissors!")
 
