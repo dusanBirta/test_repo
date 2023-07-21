e09bdbf8bfba3c8a417c79e049a63baaf50800ea
dusanBirta <31896340+dusanBirta@users.noreply.github.com>
2023-07-21 16:19:55 -0400

    Update app.py

--- app.py
+++ app.py
@@ -22,27 +22,38 @@ def main():
         # Get the middle section of the image
         middle_section = get_middle_section(image)
 
-        # Display the original image
-        st.image(image, caption="Uploaded Image", use_column_width=True)
-
         # Convert the middle section to base64 to be used in the tooltip
         _, encoded_middle_section = cv2.imencode(".png", middle_section)
         middle_section_base64 = encoded_middle_section.tobytes()
 
         # Custom HTML template for the tooltip
-        tooltip_text = "Mouse is hovering."
         tooltip_html = f"""
         <div style="position: relative; display: inline-block;">
-            <img src="data:image/png;base64,{middle_section_base64}" alt="Image" width="400" height="400">
-            <div style="visibility: hidden; width: 150px; background-color: #555; color: #fff;
+            <img src="data:image/png;base64,{middle_section_base64}" alt="Image" width="400" height="400"
+                 onmouseover="showText(event)" onmouseout="hideText(event)">
+            <div id="tooltip" style="visibility: hidden; width: 150px; background-color: #555; color: #fff;
                         text-align: center; border-radius: 6px; padding: 5px; position: absolute;
-                        z-index: 1; bottom: 120%; left: 50%; margin-left: -75px;"
-                onmouseover="this.style.visibility='visible';"
-                onmouseout="this.style.visibility='hidden';"
-            >
-                {tooltip_text}
+                        z-index: 1; bottom: 120%; left: 50%; margin-left: -75px;">
             </div>
         </div>
+        <script>
+            function showText(event) {{
+                var tooltip = document.getElementById('tooltip');
+                var image = event.target;
+                var mouseX = event.offsetX;
+                var imageWidth = image.clientWidth;
+                if (mouseX < imageWidth / 2) {{
+                    tooltip.innerHTML = 'Left';
+                }} else {{
+                    tooltip.innerHTML = 'Right';
+                }}
+                tooltip.style.visibility = 'visible';
+            }}
+            function hideText(event) {{
+                var tooltip = document.getElementById('tooltip');
+                tooltip.style.visibility = 'hidden';
+            }}
+        </script>
         """
 
         # Display the image with the tooltip
