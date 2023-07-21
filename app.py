1ff32adfad37433f55a71a2a641018aedf0960e4
dusanBirta <31896340+dusanBirta@users.noreply.github.com>
2023-07-21 16:55:03 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,8 @@
 import streamlit as st
-from streamlit.components.v1 import declare_component
 import base64
 
-# Declare the custom component with the JavaScript code
-mouse_events_component = declare_component(
-    "mouse_events",
-    url="mouse_events.js",
-)
-
 def main():
-    st.title("Mouse Events App")
+    st.title("Mouse Hover App")
 
     # File uploader to get the image from the user
     uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
@@ -23,16 +16,38 @@ def main():
             img_bytes = uploaded_file.read()
             img_base64 = base64.b64encode(img_bytes).decode()
 
-            # Pass the base64 string to the custom component
-            result = mouse_events_component(uploaded_image=img_base64)
+            # Custom HTML template for the tooltip
+            tooltip_html = f"""
+            <div style="position: relative; display: inline-block;">
+                <img src="data:image/png;base64,{img_base64}" alt="Image" width="400" height="400"
+                     onmouseover="showText(event)" onmouseout="hideText(event)">
+                <div id="tooltip" style="visibility: hidden; width: 150px; background-color: #555; color: #fff;
+                            text-align: center; border-radius: 6px; padding: 5px; position: absolute;
+                            z-index: 1; bottom: 120%; left: 50%; margin-left: -75px;">
+                </div>
+            </div>
+            <script>
+                function showText(event) {{
+                    var tooltip = document.getElementById('tooltip');
+                    var image = event.target;
+                    var mouseX = event.offsetX;
+                    var imageWidth = image.clientWidth;
+                    if (mouseX < imageWidth / 2) {{
+                        tooltip.innerHTML = 'Left';
+                    }} else {{
+                        tooltip.innerHTML = 'Right';
+                    }}
+                    tooltip.style.visibility = 'visible';
+                }}
+                function hideText(event) {{
+                    var tooltip = document.getElementById('tooltip');
+                    tooltip.style.visibility = 'hidden';
+                }}
+            </script>
+            """
 
-            # Display the dynamic updates based on mouse events
-            if result is not None:
-                if "click" in result:
-                    st.image(img_bytes, use_column_width=True, caption="Click: 'click'")
-
-                if "hovering" in result:
-                    st.image(img_bytes, use_column_width=True, caption=f"Hover: {result}")
+            # Display the image with the tooltip
+            st.components.v1.html(tooltip_html, height=400, scrolling=False)
 
 if __name__ == "__main__":
     main()
