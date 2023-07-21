0b0a158ebacff7132dc1f2fd71aa83d72409929f
dusanBirta <31896340+dusanBirta@users.noreply.github.com>
2023-07-21 16:36:11 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,32 @@
 import streamlit as st
-import cv2
-import numpy as np
+from streamlit.components.v1 import declare_component
+
+# Declare the custom component with the JavaScript code
+mouse_events_component = declare_component(
+    "mouse_events",
+    url="mouse_events.js",
+)
 
 def main():
-    st.title("Mouse Hover App")
+    st.title("Mouse Events App")
 
     # File uploader to get the image from the user
     uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
 
     if uploaded_file is not None:
-        # Read the image using OpenCV
-        image = np.array(bytearray(uploaded_file.read()), dtype=np.uint8)
-        image = cv2.imdecode(image, 1)
+        # Display the uploaded image
+        st.image(uploaded_file, use_column_width=True, caption="Uploaded Image")
 
-        # Convert the image to RGB (OpenCV uses BGR by default, but Streamlit expects RGB)
-        image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        # Pass the uploaded image to the custom component
+        result = mouse_events_component(uploaded_file)
 
-        # Display the image with mouse hover text
-        st.image(image_rgb, use_column_width=True, caption="Mouse hover")
+        # Display the dynamic updates based on mouse events
+        if result is not None:
+            if "click" in result:
+                st.image(uploaded_file, use_column_width=True, caption="Click: 'click'")
+
+            if "hovering" in result:
+                st.image(uploaded_file, use_column_width=True, caption=f"Hover: {result}")
 
 if __name__ == "__main__":
     main()
