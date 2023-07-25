7d5b005e5c0bec0c91d5668eaaaf63fc1f2b4d8a
dusanBirta <31896340+dusanBirta@users.noreply.github.com>
2023-07-25 19:16:44 -0400

    Update app.py

--- app.py
+++ app.py
@@ -42,14 +42,39 @@ def main():
         <div style="position: relative; display: inline-block;">
             <img src="data:image/png;base64,{base64.b64encode(img_bytes).decode()}" alt="Image" width="400" height="400"
                  onclick="handleClick(event)" onmousemove="handleMouseOver(event)">
-        </div>
-        <script>
-            // Your existing handleClick and handleMouseOver functions here
+            </div>
+            <script>
+                // Dictionary to store descriptions and coordinates
+                var descriptions = {{}};
 
-            // ...
-        </script>
-        <div id="message" style="position: absolute; top: 0; left: 0;"></div>
-        """
+                function handleClick(event) {{
+                    var mouseX = event.clientX;
+                    var mouseY = event.clientY;
+                    var description = prompt("Enter a description for this area:");
+                    if (description) {{
+                        descriptions[mouseX + '-' + mouseY] = description;
+                    }}
+                }}
+
+                function handleMouseOver(event) {{
+                    var mouseX = event.clientX;
+                    var mouseY = event.clientY;
+
+                    var messageDiv = document.getElementById('message');
+                    messageDiv.innerHTML = '';
+
+                    // Display descriptions for the hovered area
+                    for (var key in descriptions) {{
+                        var coords = key.split('-');
+                        var distance = Math.sqrt((mouseX - coords[0]) ** 2 + (mouseY - coords[1]) ** 2);
+                        if (distance <= 50) {{  // Only display descriptions within a radius of 50 pixels
+                            messageDiv.innerHTML += '<p style="position: absolute; left: ' + coords[0] + 'px; top: ' + coords[1] + 'px; background-color: #555; color: #fff; border-radius: 6px; padding: 5px;">' + descriptions[key] + '</p>';
+                        }}
+                    }}
+                }}
+            </script>
+            <div id="message" style="position: absolute; top: 0; left: 0;"></div>
+            """
 
         # Display the custom HTML template for the click event
         st.components.v1.html(click_html, height=400, scrolling=False)
