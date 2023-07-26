fabfe438f56411c456aab24553f1ec315152d5f5
dusanBirta <31896340+dusanBirta@users.noreply.github.com>
2023-07-26 11:52:15 -0400

    Streamlit App

---
+++ app.py
@@ -0,0 +1,85 @@
+from ultralytics import YOLO
+import streamlit as st
+import base64
+from PIL import Image
+import io
+import os
+
+
+def predict_with_yolov8(img_bytes):
+    # Load the YOLOv8 model
+    model = YOLO('best.pt')
+
+    # Convert the image bytes to PIL image
+    pil_image = Image.open(img_bytes)
+
+    # Run YOLOv8 segmentation on the image
+    results = model.predict(pil_image,imgsz=500)
+    # Get the path of the new image saved by YOLOv8
+    # Assuming inference[0] is the Results object
+    res_plotted = results[0].plot()[:, :, ::-1]
+    
+    return res_plotted
+
+def main():
+    st.title("YOLOv8 Predictions with Mouse Click App")
+
+    # File uploader to get the image from the user
+    uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
+
+    if uploaded_file is not None:
+        # Read the image as bytes
+        img_bytes = uploaded_file.read()
+
+        # Display the uploaded image
+        #st.image(img_bytes, use_column_width=True, caption="Uploaded Image")
+
+        # Custom HTML template for the click event
+        click_html = f"""
+        <div style="position: relative; display: inline-block;">
+            <img src="data:image/png;base64,{base64.b64encode(img_bytes).decode()}" alt="Image" width="400" height="400"
+                 onclick="handleClick(event)" onmousemove="handleMouseOver(event)">
+        </div>
+        <script>
+            // Dictionary to store descriptions and coordinates
+            var descriptions = {{}};
+
+            function handleClick(event) {{
+                var mouseX = event.clientX;
+                var mouseY = event.clientY;
+                var description = prompt("Enter a description for this area:");
+                if (description) {{
+                    descriptions[mouseX + '-' + mouseY] = description;
+                }}
+            }}
+
+            function handleMouseOver(event) {{
+                var mouseX = event.clientX;
+                var mouseY = event.clientY;
+
+                var messageDiv = document.getElementById('message');
+                messageDiv.innerHTML = '';
+
+                // Display descriptions for the hovered area
+                for (var key in descriptions) {{
+                    var coords = key.split('-');
+                    var distance = Math.sqrt((mouseX - coords[0]) ** 2 + (mouseY - coords[1]) ** 2);
+                    if (distance <= 50) {{  // Only display descriptions within a radius of 50 pixels
+                        messageDiv.innerHTML += '<p style="position: absolute; left: ' + coords[0] + 'px; top: ' + coords[1] + 'px; background-color: #555; color: #fff; border-radius: 6px; padding: 5px;">' + descriptions[key] + '</p>';
+                    }}
+                }}
+            }}
+        </script>
+        <div id="message" style="position: absolute; top: 0; left: 0;"></div>
+        """
+
+        # Display the custom HTML template for the click event
+        st.components.v1.html(click_html, height=400, scrolling=False)
+
+        # Predict with YOLOv8
+        seg_result = predict_with_yolov8(uploaded_file)
+
+        st.image(seg_result, use_column_width=True,caption="YOLOv8 Predictions")
+
+if __name__ == "__main__":
+    main()
