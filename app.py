1ab04cbfa7b1db6cd5b771ad3fe29eee26940a02
dusanBirta <31896340+dusanBirta@users.noreply.github.com>
2023-07-26 19:42:20 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,6 @@ import cv2
 import torch
 import numpy as np
 
-
-
 def main():
     st.title("YOLOv8 Predictions with Mouse Click App")
 
@@ -20,8 +18,11 @@ def main():
         image = Image.open(uploaded_file)
         img_array = np.array(image)
 
+        # Get image dimensions
+        height, width = img_array.shape[:2]
+
         # Load custom YOLO model named "best.pt"
-        model = YOLO('best.pt')  # Adjust the path if necessary
+        model = YOLO('best.pt')
 
         # Make predictions using the uploaded image
         results = model(img_array)
@@ -30,13 +31,14 @@ def main():
         result = results[0]
 
         # Process the result
-        boxes = result.boxes  # Boxes object for bbox outputs
+        boxes = result.boxes
 
+        # Draw bounding boxes and labels
         for (x1, y1, x2, y2, conf, class_num) in boxes.data:
             label = result.names[int(class_num)]
-            color = [int(c) for c in COLORS[int(class_num) % len(COLORS)]]  # Choose a color based on the class
-            cv2.rectangle(img_array, (int(x1), int(y1)), (int(x2), int(y2)), color, 2)
-            cv2.putText(img_array, label, (int(x1), int(y1) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+            color = [int(c) for c in COLORS[int(class_num) % len(COLORS)]]
+            cv2.rectangle(img_array, (int(x1), int(y1)), (int(x2), int(y2)), color, 3)  # Increased thickness to 3
+            cv2.putText(img_array, label, (int(x1), int(y1) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, color, 2)  # Increased font size and thickness
 
         # Convert the PIL Image to bytes
         is_success, img_buffer = cv2.imencode(".png", img_array)
@@ -46,12 +48,10 @@ def main():
         # Custom HTML template for the click event
         click_html = f"""
         <div style="position: relative; display: inline-block;">
-            <img src="data:image/png;base64,{base64.b64encode(img_bytes).decode()}" alt="Image" width="400" height="400">
+            <img src="data:image/png;base64,{base64.b64encode(img_bytes).decode()}" alt="Image" width="{width}" height="{height}">
         </div>
         <script>
-            // Dictionary to store descriptions and coordinates
             var descriptions = {{}};
-
             function handleClick(event) {{
                 var mouseX = event.clientX;
                 var mouseY = event.clientY;
@@ -60,19 +60,15 @@ def main():
                     descriptions[mouseX + '-' + mouseY] = description;
                 }}
             }}
-
             function handleMouseOver(event) {{
                 var mouseX = event.clientX;
                 var mouseY = event.clientY;
-
                 var messageDiv = document.getElementById('message');
                 messageDiv.innerHTML = '';
-
-                // Display descriptions for the hovered area
                 for (var key in descriptions) {{
                     var coords = key.split('-');
                     var distance = Math.sqrt((mouseX - coords[0]) ** 2 + (mouseY - coords[1]) ** 2);
-                    if (distance <= 50) {{  // Only display descriptions within a radius of 50 pixels
+                    if (distance <= 50) {{
                         messageDiv.innerHTML += '<p style="position: absolute; left: ' + coords[0] + 'px; top: ' + coords[1] + 'px; background-color: #555; color: #fff; border-radius: 6px; padding: 5px;">' + descriptions[key] + '</p>';
                     }}
                 }}
@@ -81,9 +77,7 @@ def main():
         <div id="message" style="position: absolute; top: 0; left: 0;"></div>
         """
 
-        # Display the custom HTML template for the click event
-        st.components.v1.html(click_html, height=400, scrolling=False)
-
+        st.components.v1.html(click_html, height=height, scrolling=False)
 
 # Define some colors for drawing bounding boxes
 COLORS = [(255, 0, 0), (0, 255, 0), (0, 0, 255), (255, 255, 0), (0, 255, 255), (255, 0, 255), (128, 128, 0), (0, 128, 128), (128, 0, 128)]
