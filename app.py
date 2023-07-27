b249e4d7935d8033eaf87c69ff34253590dd9654
dusanBirta <31896340+dusanBirta@users.noreply.github.com>
2023-07-26 20:48:15 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ import io
 import cv2
 import numpy as np
 
-
 def main():
     st.title("YOLOv8 Predictions with Mouse Click App")
 
@@ -15,45 +14,44 @@ def main():
     if uploaded_file is not None:
         image = Image.open(uploaded_file)
         img_array = np.array(image)
-        img_width, img_height = image.size
 
-        model = YOLO('best.pt')  
+        model = YOLO('best.pt')
+
         results = model(img_array)
         result = results[0]
+        boxes = result.boxes
 
-        boxes = result.boxes  
-        box_areas = []  # list to store bounding box coordinates for the JavaScript
-
-        label_font_scale = 0.75
-        box_thickness = 3
-        readable_colors = [(80, 80, 80), (80, 255, 80), (80, 80, 255), (255, 255, 80), (80, 255, 255)]
-
+        box_areas = []
         for (x1, y1, x2, y2, conf, class_num) in boxes.data:
-            box_areas.append([x1, y1, x2, y2])
             label = result.names[int(class_num)]
-            color = readable_colors[int(class_num) % len(readable_colors)]
-            cv2.rectangle(img_array, (int(x1), int(y1)), (int(x2), int(y2)), color, box_thickness)
-            cv2.putText(img_array, label, (int(x1), int(y1) - 10), cv2.FONT_HERSHEY_SIMPLEX, label_font_scale, color, 2)
+            color = [128, 128, 128]  # Change to a more readable color
+            cv2.rectangle(img_array, (int(x1), int(y1)), (int(x2), int(y2)), color, 3)
+            cv2.putText(img_array, label, (int(x1), int(y1) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.7, color, 2)
+            box_areas.append([x1, y1, x2, y2])
 
         is_success, img_buffer = cv2.imencode(".png", img_array)
         if is_success:
             img_bytes = img_buffer.tobytes()
 
+        box_areas_str = str(box_areas).replace(" ", "")
+        
         click_html = f"""
         <div style="position: relative; display: inline-block;">
-            <img src="data:image/png;base64,{base64.b64encode(img_bytes).decode()}" alt="Image" width="{img_width}" height="{img_height}" onclick="handleClick(event)" onmousemove="handleMouseOver(event)">
+            <img src="data:image/png;base64,{base64.b64encode(img_bytes).decode()}" alt="Image" style="max-width: 800px;" onclick="handleClick(event)">
         </div>
         <script>
             var descriptions = {{}};
-            var boxAreas = {box_areas};
+            var boxAreas = {box_areas_str};
 
             function handleClick(event) {{
+                console.log("Clicked");
                 var mouseX = event.offsetX;
                 var mouseY = event.offsetY;
 
                 for (var i = 0; i < boxAreas.length; i++) {{
                     var box = boxAreas[i];
                     if (mouseX >= box[0] && mouseX <= box[2] && mouseY >= box[1] && mouseY <= box[3]) {{
+                        console.log("Inside bounding box");
                         var description = prompt("Enter a description for this area:");
                         if (description) {{
                             descriptions[mouseX + '-' + mouseY] = description;
@@ -62,27 +60,10 @@ def main():
                     }}
                 }}
             }}
-
-            function handleMouseOver(event) {{
-                var mouseX = event.offsetX;
-                var mouseY = event.offsetY;
-
-                var messageDiv = document.getElementById('message');
-                messageDiv.innerHTML = '';
-
-                for (var key in descriptions) {{
-                    var coords = key.split('-');
-                    var distance = Math.sqrt((mouseX - coords[0]) ** 2 + (mouseY - coords[1]) ** 2);
-                    if (distance <= 50) {{
-                        messageDiv.innerHTML += '<p style="position: absolute; left: ' + coords[0] + 'px; top: ' + coords[1] + 'px; background-color: #555; color: #fff; border-radius: 6px; padding: 5px;">' + descriptions[key] + '</p>';
-                    }}
-                }}
-            }}
         </script>
-        <div id="message" style="position: absolute; top: 0; left: 0;"></div>
         """
-        st.components.v1.html(click_html, height=img_height, width=img_width, scrolling=False)
 
+        st.components.v1.html(click_html, height=720, scrolling=False)
 
 if __name__ == "__main__":
     main()
