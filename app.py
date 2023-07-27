8500c8f8282f811be568f6490428f129bb84c83d
dusanBirta <31896340+dusanBirta@users.noreply.github.com>
2023-07-26 20:40:48 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,73 +6,54 @@ import io
 import cv2
 import numpy as np
 
-def main():
-    st.title("Snapdetect")
 
-    # File uploader to get the image from the user
+def main():
+    st.title("YOLOv8 Predictions with Mouse Click App")
+
     uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
 
     if uploaded_file is not None:
-        # Read the image as bytes
         image = Image.open(uploaded_file)
         img_array = np.array(image)
-        height, width, _ = img_array.shape
-        
-        # Set display size
-        display_width = 800
-        display_height = int((display_width / width) * height)
+        img_width, img_height = image.size
 
-        # Load custom YOLO model named "best.pt"
-        model = YOLO('best.pt')  # Adjust the path if necessary
-
-        # Make predictions using the uploaded image
+        model = YOLO('best.pt')  
         results = model(img_array)
-
-        # Extract the first (and only) result from the list
         result = results[0]
 
-        # Process the result
-        boxes = result.boxes  # Boxes object for bbox outputs
-        box_areas = []
+        boxes = result.boxes  
+        box_areas = []  # list to store bounding box coordinates for the JavaScript
+
+        label_font_scale = 0.75
+        box_thickness = 3
+        readable_colors = [(80, 80, 80), (80, 255, 80), (80, 80, 255), (255, 255, 80), (80, 255, 255)]
 
         for (x1, y1, x2, y2, conf, class_num) in boxes.data:
-            label = result.names[int(class_num)]
-            color = [int(c) for c in COLORS[int(class_num) % len(COLORS)]]  # Choose a readable color based on the class
-            cv2.rectangle(img_array, (int(x1), int(y1)), (int(x2), int(y2)), color, 2)
-            cv2.putText(img_array, label, (int(x1), int(y1) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, color, 2)
             box_areas.append([x1, y1, x2, y2])
+            label = result.names[int(class_num)]
+            color = readable_colors[int(class_num) % len(readable_colors)]
+            cv2.rectangle(img_array, (int(x1), int(y1)), (int(x2), int(y2)), color, box_thickness)
+            cv2.putText(img_array, label, (int(x1), int(y1) - 10), cv2.FONT_HERSHEY_SIMPLEX, label_font_scale, color, 2)
 
-        # Convert the PIL Image to bytes
         is_success, img_buffer = cv2.imencode(".png", img_array)
         if is_success:
             img_bytes = img_buffer.tobytes()
 
-        # Custom HTML template for the click event
         click_html = f"""
         <div style="position: relative; display: inline-block;">
-            <img src="data:image/png;base64,{base64.b64encode(img_bytes).decode()}" alt="Image" width="{display_width}" height="{display_height}" onclick="handleClick(event)" onmousemove="handleMouseOver(event)">
+            <img src="data:image/png;base64,{base64.b64encode(img_bytes).decode()}" alt="Image" width="{img_width}" height="{img_height}" onclick="handleClick(event)" onmousemove="handleMouseOver(event)">
         </div>
         <script>
             var descriptions = {{}};
             var boxAreas = {box_areas};
 
-            var scalingFactorWidth = {width} / {display_width};
-            var scalingFactorHeight = {height} / {display_height};
-
-            function adjustCoordinates(coord, isWidth) {{
-                return isWidth ? coord / scalingFactorWidth : coord / scalingFactorHeight;
-            }}
-
-            function isInsideBox(mouseX, mouseY, box) {{
-                return mouseX >= adjustCoordinates(box[0], true) && mouseX <= adjustCoordinates(box[2], true) && mouseY >= adjustCoordinates(box[1], false) && mouseY <= adjustCoordinates(box[3], false);
-            }}
-
             function handleClick(event) {{
                 var mouseX = event.offsetX;
                 var mouseY = event.offsetY;
-                
+
                 for (var i = 0; i < boxAreas.length; i++) {{
-                    if (isInsideBox(mouseX, mouseY, boxAreas[i])) {{
+                    var box = boxAreas[i];
+                    if (mouseX >= box[0] && mouseX <= box[2] && mouseY >= box[1] && mouseY <= box[3]) {{
                         var description = prompt("Enter a description for this area:");
                         if (description) {{
                             descriptions[mouseX + '-' + mouseY] = description;
@@ -100,12 +81,8 @@ def main():
         </script>
         <div id="message" style="position: absolute; top: 0; left: 0;"></div>
         """
+        st.components.v1.html(click_html, height=img_height, width=img_width, scrolling=False)
 
-        # Display the custom HTML template for the click event
-        st.components.v1.html(click_html, height=display_height, scrolling=False)
-
-# Define some readable colors for drawing bounding boxes
-COLORS = [(128, 128, 128), (50, 168, 82), (50, 82, 168), (168, 168, 50), (50, 168, 132), (168, 50, 132), (80, 80, 0), (0, 80, 80), (80, 0, 80)]
 
 if __name__ == "__main__":
     main()
