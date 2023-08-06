290a6d29a7d0542bedf43255691aab80b0157dea
dusanBirta <31896340+dusanBirta@users.noreply.github.com>
2023-08-06 12:29:27 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,50 +1,62 @@
-import streamlit as st
-from ultralytics import YOLO
-from PIL import Image
-import cv2
 import os
+import glob
 import shutil
-import subprocess
+import cv2
+import streamlit as st
+import matplotlib.pyplot as plt
+from PIL import Image
+from ultralytics.engine.model import YOLO
 
-# YOLO face detection
-st.title("Face Detection and Enhancement")
-uploaded_file = st.file_uploader('Choose an image...', type=['jpg', 'jpeg', 'png'])
-if uploaded_file is not None:
-    # Convert the file to an opencv image.
-    image = Image.open(uploaded_file)
-    image_path = "input_image.jpg"
-    image.save(image_path)
-    model = YOLO('yolov8n-face.pt')
-    results = model(image_path)
-    im_array = results.render()[0]  # plot a BGR numpy array of predictions
-    im = Image.fromarray(im_array[..., ::-1])  # RGB PIL image
-    st.image(im, caption="Detected faces")
+# Real-ESRGAN enhancement function
+def enhance_image(input_folder='upload', outscale=3.5):
+    result_folder = 'results'
+    if os.path.isdir(result_folder):
+        shutil.rmtree(result_folder)
+    os.mkdir(result_folder)
 
-    face_counter = 0
-    original_image = cv2.imread(image_path)
-    original_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB)
-    for result in results:
-        xyxy_boxes = result.boxes.xyxy
-        for xyxy_box in xyxy_boxes:
-            x1, y1, x2, y2 = map(int, xyxy_box)
-            cropped_image = original_image[y1:y2, x1:x2]
-            face_path = f'face_{face_counter}.jpg'
-            cv2.imwrite(face_path, cv2.cvtColor(cropped_image, cv2.COLOR_RGB2BGR))
-            st.image(face_path, caption=f"Face {face_counter}")
+    # Run the Real-ESRGAN enhancement
+    os.system(f'python Real-ESRGAN/inference_realesrgan.py -n RealESRGAN_x4plus -i {input_folder} --outscale {outscale} --face_enhance')
 
-            # Enhance the face image using Real-ESRGAN
-            # Assuming Real-ESRGAN repository is cloned and set up properly
-            upload_folder = 'Real-ESRGAN/upload'
-            result_folder = 'Real-ESRGAN/results'
-            if os.path.isdir(upload_folder):
-                shutil.rmtree(upload_folder)
-            if os.path.isdir(result_folder):
-                shutil.rmtree(result_folder)
-            os.mkdir(upload_folder)
-            os.mkdir(result_folder)
-            dst_path = os.path.join(upload_folder, face_path)
-            shutil.move(face_path, dst_path)
-            subprocess.run(['python', 'Real-ESRGAN/inference_realesrgan.py', '-n', 'RealESRGAN_x4plus', '-i', upload_folder, '--outscale', '3.5', '--face_enhance'])
-            enhanced_face_path = os.path.join(result_folder, face_path)
-            st.image(enhanced_face_path, caption=f"Enhanced Face {face_counter}")
-            face_counter += 1
+    # Display results
+    input_list = sorted(glob.glob(os.path.join(input_folder, '*')))
+    output_list = sorted(glob.glob(os.path.join(result_folder, '*')))
+    for input_path, output_path in zip(input_list, output_list):
+        img_input = cv2.imread(input_path)
+        img_input = cv2.cvtColor(img_input, cv2.COLOR_BGR2RGB)
+        img_output = cv2.imread(output_path)
+        img_output = cv2.cvtColor(img_output, cv2.COLOR_BGR2RGB)
+
+        # You can modify this part to show the images in Streamlit instead of using plt
+        st.image([img_input, img_output], caption=['Input image', 'Real-ESRGAN output'])
+
+# Streamlit app
+def main():
+    st.title('YOLOv8 Face Detection & Real-ESRGAN Enhancement')
+    uploaded_file = st.file_uploader("Choose an image...", type=['jpg', 'jpeg', 'png'])
+
+    if uploaded_file is not None:
+        st.image(uploaded_file, caption='Uploaded Image.', use_column_width=True)
+        st.write("")
+        st.write("Classifying...")
+
+        upload_folder = 'upload'
+        if os.path.isdir(upload_folder):
+            shutil.rmtree(upload_folder)
+        os.mkdir(upload_folder)
+
+        file_path = os.path.join(upload_folder, uploaded_file.name)
+        with open(file_path, 'wb') as f:
+            f.write(uploaded_file.getbuffer())
+
+        # Load YOLO model
+        model = YOLO('yolov8n-face.pt')
+
+        # Detect faces
+        results = model(file_path)
+        results.show() # You can modify this to show the result in Streamlit
+
+        # Enhance detected faces using Real-ESRGAN
+        enhance_image(input_folder=upload_folder)
+
+if __name__ == '__main__':
+    main()
