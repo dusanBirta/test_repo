3fa51a7bd5929b13fe253ba83c817969ec05fa59
dusanBirta <31896340+dusanBirta@users.noreply.github.com>
2023-08-06 11:08:41 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,86 +1,57 @@
-from ultralytics import YOLO
+# app.py
 import streamlit as st
-import base64
+from ultralytics import YOLO
 from PIL import Image
-import numpy as np
 import cv2
+import numpy as np
 
-def main():
-    st.title("Snapdetect")
+# Display title and instruction
+st.title('Face Detection using YOLOv8')
+st.subheader('Upload an image to perform face detection')
 
-    # File uploader to get the image from the user
-    uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
+# Upload image
+uploaded_file = st.file_uploader('Choose an image...', type=['jpg', 'jpeg', 'png'])
+if uploaded_file is not None:
+    # Read the uploaded image
+    uploaded_image = Image.open(uploaded_file)
+    image_path = f'image_uploaded.{uploaded_file.type.split("/")[-1]}'
+    uploaded_image.save(image_path)
 
-    if uploaded_file is not None:
-        # Read the image as bytes
-        image = Image.open(uploaded_file)
-        img_array = np.array(image)
+    # Load YOLO model
+    model = YOLO('yolov8n-face.pt')
 
-        # Get image dimensions and calculate display dimensions maintaining aspect ratio
-        height, width = img_array.shape[:2]
-        display_width = 780  # Reduced a bit to prevent cut-offs
-        display_height = int((display_width / width) * height)
+    # Predict with the model
+    results = model(image_path)
 
-        # Load custom YOLO model named "best.pt"
-        model = YOLO('best.pt')
+    # Show the results
+    for r in results:
+        im_array = r.plot()  # plot a BGR numpy array of predictions
+        im = Image.fromarray(im_array[..., ::-1])  # RGB PIL image
+        st.image(im, caption='Detected faces')  # Show image in Streamlit
 
-        # Make predictions using the uploaded image
-        results = model(img_array)
-        result = results[0]  # Extract the first result
+    # Load the original image
+    original_image = cv2.imread(image_path)
+    original_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB)
 
-        # Process the result and draw bounding boxes and labels
-        boxes = result.boxes
-        for (x1, y1, x2, y2, conf, class_num) in boxes.data:
-            label = result.names[int(class_num)]
-            color = [int(c) for c in COLORS[int(class_num) % len(COLORS)]]
-            cv2.rectangle(img_array, (int(x1), int(y1)), (int(x2), int(y2)), color, 3)
-            cv2.putText(img_array, label, (int(x1), int(y1) - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, color, 3)  # Increased font size
+    # Counter for the face images
+    face_counter = 0
 
-        # Convert from BGR to RGB
-        img_array = cv2.cvtColor(img_array, cv2.COLOR_BGR2RGB)
+    # Iterate through the detection results
+    for result in results:
+        # Get bounding box coordinates in the xyxy format
+        xyxy_boxes = result.boxes.xyxy
 
-        # Convert the image array to bytes
-        is_success, img_buffer = cv2.imencode(".png", img_array)
-        img_bytes = img_buffer.tobytes() if is_success else None
+        # Crop and display each bounding box
+        for xyxy_box in xyxy_boxes:
+            # Convert the bounding box coordinates to integers
+            x1, y1, x2, y2 = map(int, xyxy_box)
 
-        # Custom HTML template for click events
-        click_html = f"""
-        <div style="position: relative; display: inline-block;">
-            <img src="data:image/png;base64,{base64.b64encode(img_bytes).decode()}" alt="Image" width="{display_width}" height="{display_height}" onclick="handleClick(event)" onmousemove="handleMouseOver(event)">
-        </div>
-        <script>
-            var descriptions = {{}};
-            function handleClick(event) {{
-                var rect = event.target.getBoundingClientRect();
-                var mouseX = event.clientX - rect.left;
-                var mouseY = event.clientY - rect.top;
-                var description = prompt("Enter a price for this item:");
-                if (description) {{
-                    descriptions[mouseX + '-' + mouseY] = description;
-                }}
-            }}
-            function handleMouseOver(event) {{
-                var rect = event.target.getBoundingClientRect();
-                var mouseX = event.clientX - rect.left;
-                var mouseY = event.clientY - rect.top;
-                var messageDiv = document.getElementById('message');
-                messageDiv.innerHTML = '';
-                for (var key in descriptions) {{
-                    var coords = key.split('-');
-                    var distance = Math.sqrt((mouseX - parseFloat(coords[0])) ** 2 + (mouseY - parseFloat(coords[1])) ** 2);
-                    if (distance <= 50) {{
-                        messageDiv.innerHTML += '<p style="position: absolute; left: ' + (rect.left + parseFloat(coords[0])) + 'px; top: ' + (rect.top + parseFloat(coords[1])) + 'px; background-color: #555; color: #fff; border-radius: 6px; padding: 5px;">' + descriptions[key] + '</p>';
-                    }}
-                }}
-            }}
-        </script>
-        <div id="message" style="position: absolute; top: 0; left: 0;"></div>
-        """
+            # Crop the image using the bounding box coordinates
+            cropped_image = original_image[y1:y2, x1:x2]
 
-        st.components.v1.html(click_html, height=display_height, scrolling=False)
+            # Convert cropped image to PIL Image and show in Streamlit
+            cropped_pil_image = Image.fromarray(cropped_image)
+            st.image(cropped_pil_image, caption=f'Face {face_counter}')
 
-# Muted colors for bounding boxes
-COLORS = [(100, 100, 100), (150, 75, 0), (75, 0, 130), (0, 75, 75), (0, 100, 0), (100, 0, 0), (50, 50, 50), (0, 50, 50), (50, 0, 50)]
-
-if __name__ == "__main__":
-    main()
+            # Increment the face counter
+            face_counter += 1
